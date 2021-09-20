789ec53ea96ff1ec7283aba4cb449b5c47c6582b
Yurii Vydai <djurker21@gmail.com>
2021-09-20 10:36:12 +0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar">
-        <a className="active" href="#home">Home</a>
+        <a className="active" href="#home">
+          Home
+        </a>
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
       </div>
 
-      <div className="content">
-        ..
-      </div>
+      <div className="content">..</div>
     </div>
   );
 }
