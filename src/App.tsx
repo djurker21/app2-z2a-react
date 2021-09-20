31da0b6e12a928377cabc5a47eebd1e791536b8a
Yurii Vydai <djurker21@gmail.com>
2021-09-20 10:35:38 +0300

    typescript fix, className

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class="sidebar">
-        <a class="active" href="#home">Home</a>
+      <div className="sidebar">
+        <a className="active" href="#home">Home</a>
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
       </div>
 
-      <div class="content">
+      <div className="content">
         ..
       </div>
     </div>
