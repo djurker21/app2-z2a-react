6743e79def94514545b2e39b5370212bb72d4269
Yurii Vydai <djurker21@gmail.com>
2021-09-20 10:44:43 +0300

    pages, styles

---
+++ src/pages/app/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Sidebar from "../sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+
+      <div className="content">..</div>
+    </div>
+  );
+}
+
+export default App;
